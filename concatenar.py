9223050355a570323b8599d4d7d032d9895a5b0c
Sonia <sonia.soleracota@alumnos.upm.es>
2018-10-10 11:58:43 +0200

    script concatenar done

---
+++ concatenar.py
@@ -0,0 +1,41 @@
+import csv, sys, operator, pprint
+import pandas as pd
+
+print "---- Script para concatenar columnas ----"
+a=0
+i=0
+lista = []
+#Si el fichero de salida no existe, lo crea
+
+if(len(sys.argv)==5):
+	columna1 = sys.argv[3]
+	columna2 = sys.argv[4]
+	ficheroin = sys.argv[1]
+	ficheroout = sys.argv[2]
+	with open(ficheroin, 'rb') as f:
+		reader = csv.reader(f)
+		for row in reader:
+			if a<1: #estamos en la primera linea
+				columna1=row.index(columna1)
+				columna2=row.index(columna2)
+				a=a+1
+			else:
+				lista.append(row[columna1]+"/"+row[columna2]) 
+	with open(ficheroin,'r') as csvinput:
+		with open(ficheroout, 'w') as csvoutput:
+			writer = csv.writer(csvoutput)
+			for row in csv.reader(csvinput):
+				if i<1:
+					i=i+1
+					writer.writerow(row+['tstamp'])
+				else:
+					writer.writerow(row+[lista[i-1]])
+	print "Hecho."
+else:
+	print "ERROR."
+	print "Uso: "
+	print "concatenar.py [FICHEROIN] [FICHEROOUT] [COLUMNA1] [COLUMNA2]"
+	print "	-- FICHEROIN es el fichero de entrada y del que se desea concatenar columnas. Su formato debe ser nombrefichero.csv"
+	print "	-- FICHEROOUT es el fichero donde se guardara el contenido de FICHEROIN y la columna nueva producto de concatenar COLUMNA1 y COLUMNA2. Si el fichero de salida no existe lo crea y su formato debe ser nombrefichero.csv"
+	
+	print "	--COLUMNA1 y COLUMNA2 son las columnas que se desean concatenar."
